82ccbff029fce140ce31d73850ed98e54b515076
SOCIETY OF <91303349+Programing-Hackers@users.noreply.github.com>
2021-10-25 14:12:47 -0500

    Add files via upload

---
+++ cono.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+# Created by: Programing_Hackers
+from pyfiglet import Figlet
+import time
+import sys
+import os
+
+def cono():
+    f = Figlet(font='slant')
+    print ("\033[35m")
+    print (f.renderText('    EL CONO'))
+    print ("\033[36m")
+    print ("""
+            MENU
+    1] AREA LATERAL
+    2] AREA TOTAL
+    3] VOLUMEN
+    4] SALIR
+    """)
+    op = int(input("Elija una opcion: "))
+    while op <= 0 or op >= 5:
+        op = int(input("Elija una opcion correcta: "))
+    if op == 1:
+        print ("AREA LATERAL")
+        pi = 3.14
+        r = int(input("Ingrese el valor del radio: "))
+        g = int(input("Ingrese el valor de la generatriz: "))
+        al = r * g * pi
+        print ("El area lateral es: {}".format(al))
+    elif op == 2:
+        print("AREA TOTAL")
+        pi = 3.14
+        r = int(input("Ingrese el valor del radio: "))
+        g = int(input("Ingrese el valor de la generatriz: "))
+        at = pi * r (r + g)
+        print ("El area total es: {}".format(at))
+    elif op == 3:
+        print ("Volumen")
+        pi = 3.14
+        r = int(input("Ingrese el valor del radio: "))
+        h = int(input("Ingrese el valor de la altura: "))
+        v = (1 / 3) * (pi * (r ** 2) * h)
+        print ("El volumen es: {}".format(v))
+    else:
+        time.sleep(0.5)
+        os.system("clear")
+        print ("Gracias")
+        print ("Vuelva Pronto :3")
+        sys.exit()
+if __name__ == "__main__":
+    cono()
